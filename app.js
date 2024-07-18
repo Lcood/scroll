c8024bd33e4ad27077ed6ea43237a64f1811851a
Lcood <fatumbd@protonmail.com>
2024-07-18 21:08:10 +0200

    scroll links and calculate height

--- app.js
+++ app.js
@@ -42,3 +42,36 @@ window.addEventListener('scroll', function () {
     topLink.classList.remove('show-link');
   }
 });
+
+// scroll sections
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    // getElementById
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    //calculate navHeight
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position -= navHeight;
+      // position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+      // position += containerHeight;
+      // linksContainer.style.height = 0;
+    }
+    // scroll to section
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
